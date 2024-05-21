7b1a26393af694acd97d818a2b5fdc504819399c
IsabellaDenStore <isabellakorkis25@gmail.com>
2024-05-21 13:34:41 +0200

    finished project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,33 @@
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginFileEncode
-);
+const rootStyles = window.getComputedStyle(document.documentElement);
 
-FilePond.setOptions({
-  stylePanelAspectRatio: 150 / 100,
-  imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-});
-FilePond.parse(document.body);
+if (
+  rootStyles.getPropertyValue("--recipe-cover-width-large") != null &&
+  rootStyles.getPropertyValue("--recipe-cover-width-large") !== ""
+) {
+  ready();
+} else {
+  document.getElementById("style-css").addEventListener("load", ready);
+}
+
+function ready() {
+  const coverWidth = parseFloat(
+    rootStyles.getPropertyValue("--recipe-cover-width-large")
+  );
+  const coverAspectRatio = parseFloat(
+    rootStyles.getPropertyValue("--recipe-cover-aspect-ratio")
+  );
+  const coverHeight = coverWidth / coverAspectRatio;
+  FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode
+  );
+
+  FilePond.setOptions({
+    stylePanelAspectRatio: 1 / coverAspectRatio,
+    imageResizeTargetWidth: coverWidth,
+    imageResizeTargetHeight: coverHeight,
+  });
+
+  FilePond.parse(document.body);
+}
